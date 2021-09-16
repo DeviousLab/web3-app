41cd517a6c660a743de90d323ee76681d7ccb0ec
DeviousLab <deviouslab@gmail.com>
2021-09-16 07:58:23 +0400

    Enabled storage of data on smart contracts
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,23 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint totalWaves;
+    mapping (address => uint) userWaves;
+
     constructor() {
         console.log("This is a test smart contract");
+    }
+    function wave() public {
+        totalWaves += 1;
+        userWaves[msg.sender] += 1;
+        console.log("%s is waved!", msg.sender);
+    }
+    function getTotalWaves() public view returns (uint) {
+        console.log("We have %d total waves", totalWaves);
+        return totalWaves;
+    }
+    function getUserWaves(address user) public view returns (uint) {
+        console.log("%s has %d waves", user, userWaves[user]);
+        return userWaves[user];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,26 @@
 async function main() {
+    const [owner, randoPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    let userWaves;
+    userWaves = await waveContract.getUserWaves(owner.address);
+
+    waveCount = await waveContract.getTotalWaves();
+    
 }
 
 main()
-    .then (() => process.exit(0))
+    .then(() => process.exit(0))
     .catch((err) => {
         console.log(err);
         process.exit(1);
